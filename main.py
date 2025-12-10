0239e497923e3a4a358c052faed78a099965f828
Nitagithubit <nitakattel87@gmail.com>
2025-12-10 15:13:38 +0545

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+# data = ['Nita','Priya','Simon']
+# data.append ('Ram')
+# print(data)
+# thislist = ["nita","priya","simon"]
+# thislist.pop(1)
+# print(thislist)
+# data = ["Ram","Sita"]
+# new = data.copy()
+# print(new)
+
+
+
