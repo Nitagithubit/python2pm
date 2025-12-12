e2972e715fd9a677525d476ee4f98db8049bf2a2
Nitagithubit <nitakattel87@gmail.com>
2025-12-12 15:12:23 +0545

    insert.py

--- insert.py
+++ insert.py
@@ -34,3 +34,43 @@
 # del a[2]
 # print (a)
 # delete garne 2 garesi hari delete vayo, o garda alex deleete hunxa, output ma
+
+# data = {
+#     "Name": "Nita",
+#     "age" : "23",
+#     "contact" : 9812342727,
+# "city" : {
+#     "Nepal": "Morang",
+# }
+# }
+# print(data["city"]["Nepal"])
+
+
+# data = [
+#     {"Name":"Priya","Age":"20"},
+#     {"Name":"Simon","Age":"18"},
+#     {"Name":[{"Username":"Admin","Address":["ktm"]}]}
+# ]
+# print(data[2]["Name"][0]["Address"][0])
+
+# # print garda [] yo bracket vayasi 0 vayo ani [] 
+# # vitra sabai 0 ma hunxa ani feri brackrt vitra vayasi arko [] huda 0 vitra 0 hunxa
+
+# data = [
+#     [{"name":"sita","city":"ktm"}],
+#     [{"name":"ram","city":"brt"}],
+# ]
+# print(data[1][0]["name"])
+# [] vayasi arko {} huda ni 0 ko 0 nai hunxa
+
+# data = {
+#     "users":[
+#         {"name":"ram","address":"ktm"},
+#     ],
+#     "students":[
+#         {"name":"sita","address":"brt"},
+#     ]
+# }
+# print(data["students"][0]["address"])
+
+# students vitra [] vayako la 0 ma vayo ani 0 vitra ko address print garda brt aauxa
