657ee32128142009979f35b3bde36c4b7db66d79
Nitagithubit <nitakattel87@gmail.com>
2025-12-15 08:26:21 +0545

    python

---
+++ demoo.py
@@ -0,0 +1,40 @@
+# What is operators?
+# Operators is a symbolic mathmatics library for python that provide a wide range of mathmatics
+
+# #Types of operator
+# #1. Arithmetic Operators: +,-,*,/,%,**,//
+# print(7+8)
+# print("Ram", "Sita") Output:Ram Sita
+# print(10/3)          Output:3.333333
+# print(10%4)          Output:2
+# print(10**5)         Output:100000
+# print(18//5)
+# num = 3+6/(5-4)
+# print("num")
+
+# 2.Comparison Operators: ==, !=, >, <, >=, <=, 
+# print(8>9) Output:False
+# print(10>4)   Output:True
+
+# 3.Logical Operators: and, or, not
+# # x=8
+# # y=4
+# # print(x>5 and y<10)         Output: True
+# # print(x>5 or y>10)          Output:True
+# # print (not(x>5 and y<10))   Output:False
+
+# # 4.Bitwise Operators: &, |, ^, ~, <<, >>
+# # print(5&6) 
+# # print(3&4)
+# # print(5&7)
+# print(bin(5))
+# print(bin(6))
+# print(3&4) 
+# print(5&3)  Output: 5=0101, 3=0011, 0101*0011= 0011:1, Result:1
+# print(5|3)  Output: 5=0101, 3=0011, 0101*0011= 0111:7, Result:7
+# print(5^3)  Output: 5=0101, 3=0011, 0101*0011= 0110:6, Result:6
+# print(5<<1) Output: 5=0101, 1=1010, RESULT 10, 5*1 Multiplying 2 times, 5*1=5, 5*1=5, result 10
+# print(6<<1) Output: result:12
+# print(5>>1) Output: 5=0101, 1=0010, 0101/0010, 5*10, 10/5=2, Result=2.
+
+# 4.Assignment Operators: =,+=,-=, /=, %=, **=, //=
