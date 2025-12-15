848b15dbfbdaee4f6fa8ea61a2d19b625a0c91a4
Nitagithubit <nitakattel87@gmail.com>
2025-12-15 15:47:39 +0545

    python

--- insert.py
+++ insert.py
@@ -104,8 +104,91 @@
 # print(amount)
 
 #p,t,r = Enter principle, time and rate
-p =20
-t =67
-r =56
-si =(p * t * r)/100
-print("simple interest=",si)
+# p =20
+# t =67
+# r =56
+# si =(p * t * r)/100
+# print("simple interest=",si)
+
+# x =5
+# y =8
+# # if x>y:
+# #      print("x is greater than y")
+# # else:
+# # #     print("x is not greater than y")     
+
+# # # # WAP to enter two person's age and print who is older. 
+# # ram = input("ram age:")
+# # shyam = input("shyam age:")
+# # # if ram>shyam:
+# # #      print("ram is greater than shyam")
+# # # else:
+# # #     print("shyam is not greater than ram")     
+
+# # nita =input("nita age:")
+# # rita =input("rita age:")
+# # if nita>rita:
+# #     print("nita is greater than rita")
+# # else:
+# #     print("rita is not greater than nita")    
+
+# # simon =input("simon age:")
+# # priya =input("priya age:")
+# # if simon>priya:
+# #     print("simon is greater than priya")
+# # else:
+# #     print("priya is not greater than simon")
+
+# x =89
+# y =76
+# # z =67
+# # if z>y and y>z:
+# #     print("z is greater than y and x")
+# # elif y>x and y>z:
+# #     print("y is greater than x and z")
+# # else: 
+# #     print("x is greater than z and y")     
+
+# x =4
+# y =34
+# u =23
+# z =34
+# if x>y and u>z:
+#     print("x is greater than y,u and z")
+# elif y>x and z>u:
+#     print ("y is greater than x,u and z")
+# elif u>x and y>z:
+#     print("u is greater than x,y and z") 
+# else:
+#     print("z is greater than x,u and y")           
+
+# num =10
+# if num%2==0:
+#     print("even number")
+# else:
+#     print("odd number")    
+
+#WAP to enter any number and check whetherbit is divisible by 3 and 5.
+# num =18
+# if num%6==0 and num%9==0:
+#     print("divisible by 6 and 9")
+# else:
+#     print("not divisible by 6 and 9")    
+
+#pre>=35 and pre<50 ->C
+# #pre>=50 and pre<65 ->B   (Question)
+# #pre>=65 and pre<80 ->A
+# #pre>=80 ->A+
+
+# percentage =int(input("Enter student percentage"))
+# if percentage >= 35 and percentage < 50:
+#     print("sita")
+# elif percentage >= 50 and percentage < 65:
+#     print("ram")   
+# elif percentage >= 65 and percentage < 80:
+#      print("shyam")
+# elif percentage >= 80: 
+#      print("gita")   
+# else:
+#     print("f")          
+
